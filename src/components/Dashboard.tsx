9fbb17ae76b388c48403e8a77ada36de2c29edd3
Daniel May <daniel@danielmay.co.uk>
2017-06-29 17:03:29 +0100

    Adding initial cards for the dashboard.

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -6,7 +6,32 @@ const Dashboard : React.SFC<{}> = () => {
         <div className="content-inner">
             <Header title="Dashboard" />
             <div className="content-dashboard">
-                Hi, I'm the dashboard!
+                <div className="uk-child-width-expand@s uk-text-center" data-uk-grid>
+                    <div>
+                        <div className="uk-card uk-card-default uk-card-body">
+                            <h1>6</h1>
+                            <p>Total Portfolios</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="uk-card uk-card-default uk-card-body">
+                            <h1>56</h1>
+                            <p>Total Sites</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="uk-card uk-card-default uk-card-body">
+                            <h1>4,583</h1>
+                            <p>Total MPANs</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="uk-card uk-card-default uk-card-body">
+                            <h1>TPI</h1>
+                            <p>Your Team</p> 
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>)
 }
