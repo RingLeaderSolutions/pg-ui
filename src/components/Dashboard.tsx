5aede61e0db358baf7712c392cfcac1d2d41279c
Daniel May <daniel@danielmay.co.uk>
2017-06-30 09:49:04 +0100

    Adding react-highcharts, highcharts packages and displaying pie chart on the dashboard page

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,7 +2,54 @@ import * as React from "react";
 import Header from "./Header";
 import CounterCard from "./CounterCard";
 
+var ReactHighCharts = require("react-highcharts");
+
 const Dashboard : React.SFC<{}> = () => {
+    var config = {
+        chart: {
+            type: 'pie'
+        },
+        title: {
+            text: ''
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: 'black'
+                    }
+                }
+            }
+        },
+        series: [{
+            name: "Portfolios",
+            colorByPoint: true,
+            data: [
+                {
+                    name: "Proposal",
+                    y: 33.33
+                }, {
+                    name: "Priced",
+                    y: 33.33,
+                    sliced: true,
+                    selected: true
+                }, {
+                    name: "Qualified",
+                    y: 16.66
+                }, {
+                    name: "Indicative Pricing Only",
+                    y: 16.66
+                }]
+        }]
+    };
+
     return (
         <div className="content-inner">
             <Header title="Dashboard" />
@@ -12,6 +59,14 @@ const Dashboard : React.SFC<{}> = () => {
                     <CounterCard title="56" label="Total Sites" />
                     <CounterCard title="4,583" label="Total MPANs" />
                     <CounterCard title="TPI" label="Your Team" />
+                </div>
+                <div className="uk-child-width-expand@s uk-text-center" data-uk-grid>
+                    <div>
+                        <div className="uk-card uk-card-default uk-card-body">
+                            <h3>Portfolio Status</h3>
+                            <ReactHighCharts config={config} />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>)
