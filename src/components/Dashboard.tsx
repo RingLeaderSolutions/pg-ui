2e8346d5cad81f6b0e82e7364343d31e577aa9b3
Daniel May <daniel@danielmay.co.uk>
2017-06-30 11:07:39 +0100

    Adding Portfolio Timeline table to the dashboard page

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -67,6 +67,47 @@ const Dashboard : React.SFC<{}> = () => {
                             <ReactHighCharts config={config} />
                         </div>
                     </div>
+                    <div>
+                        <div className="uk-card uk-card-default uk-card-body">
+                            <h3>Portfolio Timeline</h3>
+                            <table className="uk-table uk-table-divider">
+                                <thead>
+                                    <tr>
+                                        <th>Contract Dates</th>
+                                        <th>Portfolios</th>
+                                        <th>MPAN Workload</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1st October 2017</td>
+                                        <td><strong>Quantum Analysis 17/18</strong></td>
+                                        <td><span className="uk-label uk-label-danger">487</span></td>
+                                    </tr>
+                                    <tr>
+                                        <td>1st April 2018</td>
+                                        <td><strong>At Energy 18/19</strong></td>
+                                        <td><span className="uk-label uk-label-warning">126</span></td>
+                                    </tr>
+                                    <tr>
+                                        <td></td>
+                                        <td><strong>Capalona 18/19</strong></td>
+                                        <td><span className="uk-label uk-label-warning">65</span></td>
+                                    </tr>
+                                    <tr>
+                                        <td>1st July 2018</td>
+                                        <td><strong>Zenergi Ltd 18/19</strong></td>
+                                        <td><span className="uk-label uk-label-danger">382</span></td>
+                                    </tr>
+                                    <tr>
+                                        <td>1st April 2019</td>
+                                        <td><strong>Eco Elite 19/20</strong></td>
+                                        <td><span className="uk-label uk-label-warning">23</span></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>)
