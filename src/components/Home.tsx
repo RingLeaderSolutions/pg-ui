789d375014dd1cec621968311681dcd328b32325
Daniel May <daniel.may@ringleadersolutions.com>
2018-05-15 14:31:27 -0500

    Upping UI version to v0.1.21

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -37,7 +37,7 @@ class Home extends React.Component<StateProps & DispatchProps, {}> {
                         <div className="app-title">
                             <img src={require('../images/tpi-flow-logo.png')} alt="TPI Flow" /> 
                             <div className="environment">
-                                <span className="uk-label">{appConfig.environment_name} v0.1.20</span>
+                                <span className="uk-label">{appConfig.environment_name} v0.1.21</span>
                             </div>
                             <div className="environment">
                                 <span className="uk-label">Backend v{this.props.backendVersion}</span>
