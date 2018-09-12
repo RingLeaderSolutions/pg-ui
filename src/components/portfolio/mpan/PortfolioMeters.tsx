acf280f19d2bae110d2515ecd630a7ed3d702262
Daniel May <daniel.may@ringleadersolutions.com>
2018-09-12 13:49:35 -0500

    Implementing search capability across both Electricity and Gas in the PortfolioMeters screen

--- src/components/portfolio/mpan/PortfolioMeters.tsx
+++ src/components/portfolio/mpan/PortfolioMeters.tsx
@@ -40,7 +40,24 @@ interface MeterTableEntry {
     [key: string]: any;
 }
 
-class PortfolioMeters extends React.Component<PortfolioMetersProps & StateProps & DispatchProps, {}> {
+interface PortfolioMetersState{
+    searchText: string;
+    utility: UtilityType;
+    tableData: MeterTableEntry[];
+    tableColumns: Column[];
+}
+
+class PortfolioMeters extends React.Component<PortfolioMetersProps & StateProps & DispatchProps, PortfolioMetersState> {
+    constructor(){
+        super();
+        this.state = {
+            searchText: '',
+            utility: UtilityType.Electricity,
+            tableData: [],
+            tableColumns: []
+        }
+    }
+
     componentDidMount(){
         if(this.props.portfolio != null){
             this.props.fetchMeterConsumption(this.props.portfolio.id);      
@@ -49,6 +66,87 @@ class PortfolioMeters extends React.Component<PortfolioMetersProps & StateProps 
 
     exportMeterConsumption(){
         this.props.exportMeterConsumption(this.props.portfolio.id);
+    }
+
+    componentWillReceiveProps(nextProps: PortfolioMetersProps & StateProps & DispatchProps){
+        if(nextProps.consumption == null){
+            return;
+        }
+
+        var utility = nextProps.selectedTab == 0 ? UtilityType.Electricity : UtilityType.Gas;
+        var searchText = this.props.selectedTab == nextProps.selectedTab ? this.state.searchText : "";
+
+        var tableColumns = this.createTableColumns(nextProps.consumption, utility);
+        var tableData = this.filterMeters(nextProps.consumption, utility, searchText)
+
+        this.setState({
+            ...this.state,
+            searchText,
+            utility,
+            tableColumns,
+            tableData
+        })
+    }
+
+    filterMeters(consumption: MeterConsumptionSummary, utility: UtilityType, searchText: string){
+        var rawData = utility == UtilityType.Electricity ? consumption.electrictyConsumptionEntries : consumption.gasConsumptionEntries;
+        var tableData : MeterTableEntry[] = this.createTableData(rawData)
+        if(searchText == null || searchText == ""){
+            return tableData;
+        }
+
+        var lowerSearchText = searchText.trim().toLocaleLowerCase();
+        var filtered = tableData.filter(meterEntry => {
+            var match = false;
+
+            var keys = Object.keys(meterEntry);
+            keys.forEach(property => {
+                var value: string = meterEntry[property] as string;
+                if(value && value.toLocaleLowerCase().includes(lowerSearchText)){
+                    match = true;
+                }
+            });
+            return match;
+        });
+
+        return filtered;
+    }
+
+    createTableColumns(consumption: MeterConsumptionSummary, utility: UtilityType): Column[] {
+        var headers = utility == UtilityType.Electricity ? consumption.electricityHeaders : consumption.gasHeaders;
+        return headers.map((c, index) => {
+            return {
+                Header: c,
+                accessor: String(index)
+            }
+        });
+    }
+
+    createTableData(values: string[][]): MeterTableEntry[] {
+        return values.map(rowArray => {
+            var rowObject: MeterTableEntry = {};
+            for(var i = 0; i < rowArray.length; i++){
+                var accessor = String(i);
+                rowObject[accessor] = rowArray[i];
+            }
+            rowObject['core'] = rowArray[1];
+            return rowObject;
+         });
+    }
+
+    handleSearch(ev: any){
+        var raw = String(ev.target.value);
+        if(this.state.searchText === raw){
+            return;
+        }
+
+        var tableData: MeterTableEntry[] = this.filterMeters(this.props.consumption, this.state.utility, raw);
+
+        this.setState({
+            ...this.state,
+            searchText: raw,
+            tableData
+        });
     }
 
     excludeMeter(event: any, mpanCore: string){
@@ -62,50 +160,28 @@ class PortfolioMeters extends React.Component<PortfolioMetersProps & StateProps 
     }
 
     renderDynamicTable(columns: string[], values: string[][], utilityType: UtilityType){
-        var tableColumns: Column[] = columns.map((c, index) => {
-            return {
-                Header: c,
-                accessor: String(index)
-            }
-        });
-
-        tableColumns.push({
-            Header: '',
-            accessor: 'core',
-            sortable: false,
-            Cell: row => {
-                var mpanCore = row.value;
-                return (<button className='uk-button uk-button-default uk-button-small borderless-button' onClick={(ev) => this.excludeMeter(ev, mpanCore)}><i className="fas fa-folder-minus" data-uk-tooltip="title: Exclude"></i></button>)   
-            }
-        });
-
-        var data: MeterTableEntry[] = values.map(rowArray => {
-           var rowObject: MeterTableEntry = {};
-           for(var i = 0; i < rowArray.length; i++){
-               var accessor = String(i);
-               rowObject[accessor] = rowArray[i];
-           }
-           rowObject['core'] = rowArray[1];
-           return rowObject;
-        });
-
         var decodedUtilityType = decodeUtilityType(utilityType);
         var includeDialogId = `include_meters_${decodedUtilityType}`;
         var excludeDialogId = `exclude_meters_${decodedUtilityType}`;
 
-        var hasData = data != null && data.length > 0;
+        var hasData = this.state.tableData != null && this.state.tableData.length > 0;
 
         var showHistoricUpload = hasData && values.filter(arr => arr[2] == "HH").length > 0;
         return (
             <div>
                 <div>
-                    <div className="uk-grid" data-uk-grid>
-                        <div className="uk-width-expand@s">
+                    <div className="uk-grid uk-grid-small" data-uk-grid>
+                        <div className="uk-width-expand uk-flex uk-flex-middle">
+                            <div className="uk-inline uk-width-1-1">
+                                <span className="uk-form-icon" data-uk-icon="icon: search"></span>
+                                <input className="uk-input" placeholder="Search..." value={this.state.searchText} onChange={(e) => this.handleSearch(e)}/>
+                            </div>
                         </div>
-                        <div className="uk-width-auto@s">
-                        { utilityType == UtilityType.Electricity ? (<button className='uk-button uk-button-primary uk-margin-small-left uk-margin-small-right' onClick={() => this.props.openModalDialog('upload_consumption')} disabled={!showHistoricUpload}><i className="fa fa-file-upload uk-margin-small-right fa-lg"></i>Upload Historic Consumption</button>) : null}
-                        </div>
-                        <div className="uk-width-auto@s">
+                        { utilityType == UtilityType.Electricity ?
+                            (<div className="uk-width-auto uk-flex uk-flex-middle">
+                                <button className='uk-button uk-button-primary uk-margin-small-left uk-margin-small-right' onClick={() => this.props.openModalDialog('upload_consumption')} disabled={!showHistoricUpload}><i className="fa fa-file-upload uk-margin-small-right fa-lg"></i>Upload Historic Consumption</button>
+                            </div>) : null }
+                        <div className="uk-width-auto uk-flex uk-flex-middle">
                             <div className="uk-inline">
                                 <button className="uk-button uk-button-default uk-margin-small-right borderless-button" type="button">
                                     <i className="fa fa-ellipsis-v"></i>
@@ -136,8 +212,8 @@ class PortfolioMeters extends React.Component<PortfolioMetersProps & StateProps 
                 {hasData ? (
                     <ReactTable 
                     showPagination={false}
-                    columns={tableColumns}
-                    data={data}
+                    columns={this.state.tableColumns}
+                    data={this.state.tableData}
                     style={{maxHeight: `${window.innerHeight - 320}px`}}
                     minRows={0}/>
                 ) : 
