065368881e33cd64b05e8b86ef8b7d3fb0dd9842
Daniel May <daniel.may@ringleadersolutions.com>
2018-10-29 16:51:32 -0500

    :zap: Ordering accounts alphabetically by company name for display in new portfolio dialog, etc

--- src/app/reducers/hierarchy/retrieveAccountsReducer.ts
+++ src/app/reducers/hierarchy/retrieveAccountsReducer.ts
@@ -12,11 +12,17 @@ const retrieveAccountsReducer = requestResponseReducer(
     types.RETRIEVE_ACCOUNTS_SUCCESSFUL,
     types.RETRIEVE_ACCOUNTS_FAILED,
     (state, action) => {
+        let accounts = action.data as Account[];
         return {
             ...state,
             working: false,     
             error: false,       
-            value: action.data
+            value: accounts.sort(
+                (account1: Account, account2: Account) => {            
+                    if (account1.companyName.toLowerCase() < account2.companyName.toLowerCase()) return -1;
+                    if (account1.companyName.toLowerCase() > account2.companyName.toLowerCase()) return 1;
+                    return 0;
+                })
         };
     }
 );
